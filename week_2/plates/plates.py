862a946b19a5d8c1f445ac033a3454340d577d8f
SHAKHZOD <shoxzodohunjonov@gmail.com>
2025-06-17 05:24:54 +0800

    week_2 plates.py

--- week_2/plates/plates.py
+++ week_2/plates/plates.py
@@ -23,9 +23,34 @@ def is_valid(plate):
                 p+1
         return True
 
-def word_letters_converter(word):
-    word=word.replace("", " ")
-    letters=word.split()
-    return letters
-main()            
-     
+main() 
+#another version with for-in loop
+
+def main_2():
+    plate= input(f"input: ")
+    if is_valid(plate):
+        print(f"Valid")
+    else:
+        print("Invalid")    
+
+def is_valid(plate):
+    if plate.count(" ")>0:
+        return False
+    elif (1<len(plate)<7)==False:
+        return False
+    elif str(plate[0:2]).isalpha()==False:
+        return False
+    elif str(plate).isalnum()==False:
+        return False
+    elif plate.isalpha():
+        return True
+    else:
+        space=" "
+        z=""
+        for char in plate:
+            if str(char).isdigit():
+                z=z+"1"
+        return not (space in z.strip())
+
+main_2() 
+
