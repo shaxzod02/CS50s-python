6a8bbf7c247f7e1156d02551d42b11873621e3eb
SHAKHZOD <shoxzodohunjonov@gmail.com>
2025-06-16 12:41:55 +0800

    first commit

--- week_2/twttr/twttr.py
+++ week_2/twttr/twttr.py
@@ -1,5 +1,17 @@
 def main():
-    input(f"input: ")
-    print(f"output: ")
+    user_input=input(f"input: ")
+    print(f"output: {shortten_converter(user_input)} ")
+def shortten_converter(word):
+    vowels=("a", "e", "i", "o", "u")
+    word=word.replace("", " ")
+    letters=word.split()
+    p=0
+    while p<len(letters):
+        if str (letters[p]).lower in vowels:
+            letters[p]=""
+            p+=1
+        else:
+            p+=1
+    return "".join(letters)            
 
 main()
